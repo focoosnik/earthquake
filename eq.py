7ef5cd7ad5f6007ff2abfe477d3e20d43b8a7884
focoosnik <albiorix@bk.ru>
2022-02-17 04:03:40 +0300

    output in widget listbox

--- eq.py
+++ eq.py
@@ -1,6 +1,10 @@
 import requests
 import sqlite3
+from tkinter import *
 
+main_form = Tk()
+main_form.geometry('500x500')
+main_form.title('Found EQs')
 params_dict = {'format':'geojson',
                'starttime':'2019-01-01',
                'endtime':'2019-05-01',
@@ -18,7 +22,7 @@ def request_eq(params):
     for i in range(0,len(data['features'])):
         place = data['features'][i]['properties']['place']
         mag = data['features'][i]['properties']['mag']
-        print(f'{i}. Place {place} with {mag}')
+        # print(f'{i}. Place {place} with {mag}')
         results_eq.append((i, place, mag))
     return results_eq
 
@@ -42,23 +46,28 @@ def print_eq_from_db(table_name):
     conn = sqlite3.connect("results_eq.db")
     cursor = conn.cursor()
     select_query = f"SELECT * FROM {table_name};"
+    result_box = Listbox(main_form)
+    result_box.pack(fill=X)
     cursor.execute(select_query)
-    # print(cursor.fetchall())
+    for i in cursor.fetchall():
+        result_box.insert(END, i)
+    print(cursor.fetchall())
     conn.commit()
     conn.close()
 
 
 close_app = None
 while close_app !='0':
-    params_dict['starttime'] = str(input("Enter the start time yyyy-mm-dd(example: 2019-01-01)= "))
-    params_dict['endtime'] = str(input("Enter the start time yyyy-mm-dd(example: 2019-05-01)= "))
-    params_dict['latitude'] = str(input("Enter the latitude (example: 51.51)= "))
-    params_dict['longitude'] = str(input("Enter the longitude (example: -0.12)= "))
-    params_dict['maxradiuskm'] = str(input("Enter the max radius in km (example: 2000)= "))
-    params_dict['minmagnitude'] = str(input("Enter the min magnitude (example: 2)= "))
+    # params_dict['starttime'] = str(input("Enter the start time yyyy-mm-dd(example: 2019-01-01)= "))
+    # params_dict['endtime'] = str(input("Enter the start time yyyy-mm-dd(example: 2019-05-01)= "))
+    # params_dict['latitude'] = str(input("Enter the latitude (example: 51.51)= "))
+    # params_dict['longitude'] = str(input("Enter the longitude (example: -0.12)= "))
+    # params_dict['maxradiuskm'] = str(input("Enter the max radius in km (example: 2000)= "))
+    # params_dict['minmagnitude'] = str(input("Enter the min magnitude (example: 2)= "))
     filename = input ('Enter tablename for results: ')
     save_eq(filename, request_eq(params_dict))
     print_eq_from_db(filename)
+    main_form.mainloop()
     close_app = str(input('Enter "0" for close APP or any key for continue  '))
 
 

ac6343241647115a9b7f4f0d8a1c4b48b9456c36
focoosnik <albiorix@bk.ru>
2022-03-24 03:25:50 +0300

    Added new widgets (lat, long, max radius)

--- uipyqt5.py
+++ uipyqt5.py
@@ -8,57 +8,89 @@ class MainForm(QMainWindow):
     def __init__(self):
         super().__init__()
         # Window
-        self.setWindowTitle('Data about earthquake')
-        self.setMinimumWidth(300)
+        self.setWindowTitle('Information about earthquake')
+        self.setMinimumWidth(350)
+        self.setMaximumWidth(350)
         self.setMinimumHeight(200)
+        self.setMaximumHeight(200)
         self.setWindowIcon(QIcon('icon.jpg'))
+        self.statusBar().showMessage('Ready')
         # Start time
         self.lblStarttime = QLabel('Start time', self)
         self.lblStarttime.setFont(QFont('Cali', 10))
         self.lblStarttime.move(10, 0)
 
         self.dStarttime = QDateEdit(QDate().currentDate(), self)
-        self.dStarttime.move(20, 25)
+        self.dStarttime.move(10, 25)
         self.dStarttime.setCalendarPopup(True)
 
         self.dStarttime.dateChanged.connect(self.LimitStartTime)
         # End time
         self.lblEndtime = QLabel('End time', self)
         self.lblEndtime.setFont(QFont('Cali', 10))
-        self.lblEndtime.move(10, 55)
+        self.lblEndtime.move(10, 60)
 
         self.dEndtime = QDateEdit(QDate().currentDate(), self)
-        self.dEndtime.move(20, 80)
+        self.dEndtime.move(10, 85)
         self.dEndtime.setCalendarPopup(True)
 
         self.dEndtime.dateChanged.connect(self.LimitEndTime)
         # Checkbox 'show results'
-        self.cbShowres =QCheckBox('Snow results in window', self)
+        self.cbShowres = QCheckBox('Snow results in window', self)
         self.cbShowres.toggle()
-        self.cbShowres.move(140, 0)
+        self.cbShowres.move(140, 120)
         self.cbShowres.setMinimumWidth(150)
+        # Line edit lattitude
+        self.lblLat = QLabel('Lattitude', self)
+        self.lblLat.setFont(QFont('Cali', 10))
+        self.lblLat.setMinimumWidth(120)
+        self.lblLat.move(140, 0)
+
+        self.leLat = QLineEdit('64', self)
+        self.leLat.move(140, 25)
+
+        self.leLat.textChanged.connect(self.LimitLat)
+        # Line edit longitude
+        self.lblLong = QLabel('Longitude', self)
+        self.lblLong.setFont(QFont('Cali', 10))
+        self.lblLong.setMinimumWidth(120)
+        self.lblLong.move(140, 60)
+
+        self.leLat = QLineEdit('64', self)
+        self.leLat.move(140, 85)
+        # Line edit Maxradiuskm
+        self.lblMaxrad = QLabel('Max. radius km', self)
+        self.lblMaxrad.setFont(QFont('Cali', 10))
+        self.lblMaxrad.setMinimumWidth(120)
+        self.lblMaxrad.move(10, 120)
+
+        self.leMaxrad = QLineEdit('60', self)
+        self.leMaxrad.move(10, 145)
 
         #self.cbShowres.stateChanged.connect()
         # Get data button
         self.btnGet = QPushButton('Get data', self)
-        self.btnGet.move(140, 150)
+        self.btnGet.move(140, 145)
+        self.btnGet.setStyleSheet("background-color: #99ff99")
 
         #self.getBtn.clicked.connect(self.setColor)
         # Min magnitude
-        self.lblMinMag = QLabel('Min magnitude = 0', self)
+        self.lblMinMag = QLabel('Mag. > 0', self)
         self.lblMinMag.setFont(QFont('Cali', 10))
         self.lblMinMag.setMinimumWidth(120)
-        self.lblMinMag.move(10, 110)
+        self.lblMinMag.move(270, 0)
 
-        self.cldMinMag = QSlider(Qt.Horizontal, self)
+        self.cldMinMag = QSlider(Qt.Vertical, self)
+        self.cldMinMag.setMinimumHeight(140)
+        self.cldMinMag.setMaximumWidth(25)
         self.cldMinMag.setMaximum(90)
-        self.cldMinMag.move(20, 130)
+        self.cldMinMag.move(280, 35)
         self.cldMinMag.setTickPosition(2)
         self.cldMinMag.sliderMoved.connect(self.MinMag)
 
 
     def MinMag(self, value):
-        self.lblMinMag.setText(f'Min magnitude {str(value/10)}')
+        self.lblMinMag.setText(f'Mag. > {str(value/10)}')
 
 
     def LimitEndTime(self, date):
@@ -73,6 +105,12 @@ class MainForm(QMainWindow):
         limit = self.dEndtime.date()
         if data > limit:
             self.dStarttime.setDate(limit)
+
+    def LimitLat(self, value):
+        if str(value).isdigit():
+            self.statusBar().showMessage('true')
+        else: self.statusBar().showMessage('false')
+
 
 if __name__ == '__main__':
     app_main = QApplication(sys.argv)
